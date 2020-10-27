8331797fc953dbe55738cb6c62be972d5daa8974
PineappleRind <pineapplerind.info@gmail.com>
2020-10-27 15:34:10 -0400

    Random word and content added

--- main.js
+++ main.js
@@ -146,6 +146,22 @@
             return Math.floor(Math.random() * (max - min + 1)) + min;
         }
 
+        function getRandomContent() {
+            var letters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890';
+            var content = '';
+            for (var i = 0; i < getRandomInt(5, 18); i++) {
+              content += letters[Math.floor(Math.random() * 62)];
+            }
+            return content;
+        }
+
+        function getRandomWord() {
+            var words = ['grape', 'pitch', 'judgement','toll','gain','brag','committee','tap','jam','information','break','discovery','mosquito','tempt','mountain','south','west','north','south','inside','salt','pepper','pest','establish','if'];
+            var word = '';
+              word += words[Math.floor(Math.random() * words.length)];
+            return word;
+        }
+
         function random() {
             let div = document.getElementById("div");
 
@@ -162,7 +178,7 @@
             let fontFamilies = ['Georgia, serif', ' Times New Roman, serif', 'Helvetica, sans-serif', 'Zapfino, cursive', 'Noto Sans JP, sans-serif', 'Comic Sans MS, cursive', 'Verdana, sans-serif', 'Courier, monospace', 'Grandstander, sans-serif']
 
             divHeight.value = getRandomInt(10,101)
-            divWidth.value = getRandomInt(10,101)
+            divWidth.value = getRandomInt(30,101)
             divBorderRadius.value = Math.floor(Math.random() * 101);
             divBgColor.value = getRandomColor();
             divBorderThickness.value = Math.floor(Math.random() * 101);
@@ -175,7 +191,27 @@
             if (divFontColor.value === divBgColor.value) {
                 divFontColor.value = getRandomColor();
             }
+
+            let randomContent = document.getElementById('randomContent');
+            let randomWord = document.getElementById('randomWord');
+            let errorMessage = document.getElementById('randomErrorMessage');
+
+            if (randomContent.checked === true && randomWord.checked === false) {
+                div.innerHTML = getRandomContent();
+                errorMessage.style.display = 'none';
+            } else if (randomWord.checked === true && randomContent.checked === false) {
+                div.innerHTML = getRandomWord();
+                errorMessage.style.display = 'none';
+            } else if (randomWord.checked === false && randomContent.checked === false) {
+                div.innerHTML = '';
+                errorMessage.style.display = 'none';
+            } else {
+                 errorMessage.style.display = 'block';
+                 div.innerHTML = '';
+            }
         }
+
+
 
         function reset() {
             window.location.href = "https://div-styler.netlify.app";
